eab0af8dda237f5c8090555a834e8a4d659ea5dd
Merishna Singh Suwal <merishna97@gmail.com>
2018-01-21 08:15:35 +0545

    Update script.js

--- browser-extension/script.js
+++ browser-extension/script.js
@@ -3,18 +3,25 @@ document.addEventListener('mouseover',(e) => {
     var target= e.target;
     var msgtext=target.innerText;
     var msg = new SpeechSynthesisUtterance(msgtext);
-
+    var imgtext=target.getElementsByTagName("img").alt;
     target.classList.toggle("speakText");
-
+    
     if(target.classList.contains("speakText")){    
-        document.addEventListener('click',(e) => {    
+        document.addEventListener('click',() => {    
             speechSynthesis.cancel();
         });
+        // console.log(e);
+        // if(imgtext){
+        // speechSynthesis.speak(imgtext);
+        // }
+        speechSynthesis.speak(msg);
+        
         // console.log(msgtext.indexOf("↵"));
         //if(msgtext.indexOf(/\n) != null){
             // console.log(e.target);
             // console.log(msgtext.indexOf(". "));
-       speechSynthesis.speak(msg);
+       
+
         
     // }
 }
