35c6775bdfeff11e499ef4d4fc8ac9df68c486a9
merishnaSuwal <merishna97@gmail.com>
2018-01-24 16:44:06 +0545

    Added out/in frame notification

--- browser-extension/script.js
+++ browser-extension/script.js
@@ -1,35 +1,58 @@
 var tagList = ['HTML', 'HEAD', 'BODY', 'DIV'];
 
+$(document).mouseleave(function(){
+    var ooftext = "Out of frame";
+    var oofmsg = new SpeechSynthesisUtterance(ooftext);
+    speechSynthesis.speak(oofmsg);
+});
+
+$(document).mouseenter(function(){
+    var oofstext = "Inside frame";
+    var oofsmsg = new SpeechSynthesisUtterance(oofstext);
+    speechSynthesis.speak(oofsmsg);
+});
+
 $(document).mouseover(function (e) {
     var target = $(e.target);
-    if(tagList.indexOf(target.prop("tagName")) == -1){
-        var msgtext = target.text();
-        var msg = new SpeechSynthesisUtterance(msgtext);
-        var msgalt = target.attr("alt");
-        var msgaltnew = new SpeechSynthesisUtterance(msgalt);
-        console.log(msgalt);
-        var msglabel= target.attr('aria-label');
-        var msglabelnew = new SpeechSynthesisUtterance(msglabel);
+    var msgtext = target.text();
+    var msg = new SpeechSynthesisUtterance(msgtext);
+    var msgalt = target.attr("alt");
+    var msgaltnew = new SpeechSynthesisUtterance(msgalt);
+    var msglabel= target.attr('aria-label');
+    var msglabelnew = new SpeechSynthesisUtterance(msglabel);
+    function speaker()
+    {
+        speechSynthesis.speak(msg);
+        speechSynthesis.speak(msgaltnew);
+        speechSynthesis.speak(msglabelnew);
+    }
 
-        target.addClass("speakText");
-        selectedTarget = target;
-        setTimeout(function(){
-            $(selectedTarget).mouseleave(function(){
-                selectedTarget.removeClass("speakText");
-                speechSynthesis.cancel();
-            });
-        },100); 
-
+    function stopSpeaker()
+    {
+        target.removeClass("speakText");
+        speechSynthesis.cancel();
+    }
+   
+    function classCheck()
+    {
         if(target.is(".speakText") ) {
-            speechSynthesis.speak(msg);
-            speechSynthesis.speak(msgaltnew);
-            speechSynthesis.speak(msglabelnew);
+            speaker();
             var isSpeaking=true;
             if(isSpeaking) {
                 $(document).click(function() {
-                speechSynthesis.cancel();
+                    stopSpeaker();
                 });
             } 
         }
     }
+    // console.log(tagList.indexOf(target.prop("tagName")));
+    if(tagList.indexOf(target.prop("tagName")) == -1){
+            target.addClass("speakText");
+            setTimeout(function(){
+                $(target).mouseleave(function(){
+                    stopSpeaker();
+                });
+            },100);
+            classCheck();         
+        } 
 });
